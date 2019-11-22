da4ccc4e06d2a94a01f4d74d46b15a4de12a7ff8
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2019-11-22 19:17:01 -0300

    Adicionando programa em RBF

---
+++ P1/teste.rkt
@@ -0,0 +1,9 @@
+#lang reader "reader.rkt"
+
+A maior linguagem de todos os tempos!
+
+Exercicio @
+inc 14 dec inc dec inc dec inc begin fwd inc dec inc dec inc dec inc rwd dec end fwd write
+
+++++-+++-++-++[>++++-+++-++-++<-]>.
+
