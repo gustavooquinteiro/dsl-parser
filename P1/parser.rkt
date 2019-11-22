90a0d60feb345ac8b696961c80175e058878e8dd
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2019-11-22 19:14:38 -0300

    Adicionando gramática do RBF

--- P1/parser.rkt
+++ P1/parser.rkt
@@ -1,1 +1,8 @@
- 
+#lang brag
+
+rbf-program: (rbf-op | rbf-loop)*
+rbf-op: rbf-op-n | WRITE | READ
+rbf-op-n: FWD [integer] | RWD [integer] | INC [integer] | DEC [integer]
+rbf-loop: BEGIN (rbf-op | rbf-loop)* END
+integer: digit+
+digit: ONE | TWO | THREE | FOUR | FIVE | SIX | SEVEN | EIGHT | NINE
