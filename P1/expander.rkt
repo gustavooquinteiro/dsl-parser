a2c9cf1982cedb4225ce36fd6ac2576ee9c5161c
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2019-11-22 20:01:10 -0300

    Iniciando a codificação do expansor

--- P1/expander.rkt
+++ P1/expander.rkt
@@ -1,7 +1,20 @@
 #lang br/quicklang
 
-(define-macro (bf-module-begin PARSE-TREE)
-  #'(#%module-begin 'PARSE-TREE))
+(define-macro (rbf-module-begin PARSE-TREE)
+  #'(#%module-begin PARSE-TREE))
 
 (provide (rename-out 
-            [bf-module-begin #%module-begin]))
+            [rbf-module-begin #%module-begin]))
+
+
+(define-macro (rbf-program OPERATION-OR-LOOP-ARGS ...)
+  #'(void OPERATION-OR-LOOP-ARGS ...))
+
+(provide rbf-program)
+
+(define-macro (rbf-loop "[" OPERATION-OR-LOOP-ARGS ... "]")
+  #'(until (zero? (current-byte))
+      OPERATION-OR-LOOP-ARGS ...))
+
+(provide rbf-loop)
+
