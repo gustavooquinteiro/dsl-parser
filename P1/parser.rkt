546a6483bcd76d761735329ad9713ba5dfe97cf4
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2019-11-29 06:14:10 -0300

    Alterando gramática do parser

--- P1/parser.rkt
+++ P1/parser.rkt
@@ -1,7 +1,5 @@
 #lang brag
 
 rbf-program: (rbf-op | rbf-loop)*
-rbf-op: FWD [integer] | RWD [integer] | INC [integer] | DEC [integer] | WRITE | READ
+rbf-op: FWD [INTEGER] | RWD [INTEGER] | INC [INTEGER] | DEC [INTEGER] | WRITE | READ
 rbf-loop: BEGIN (rbf-op | rbf-loop)* END
-integer: digit+
-digit: INTEGER
