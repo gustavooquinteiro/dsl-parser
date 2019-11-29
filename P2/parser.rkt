86e9b87c0a0a7485e83e2dac832d972bf220f992
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2019-11-29 18:44:35 -0300

    Adicionando a gramática MIPS

--- P2/parser.rkt
+++ P2/parser.rkt
@@ -1,5 +1,7 @@
 #lang brag
 
-mips-program: (mips-loop | mips-instruction)*
-mips-instruction: ADD (BR | CR | DR) INTEGER | SUB (BR | CR | DR) INTEGER | WRITE
-mips-loop: MIPS-LOOP
+mips-program: (mips-instruction)*
+mips-instruction: ADD (BR | CR | DR) (BR | CR | DR) (BR | CR | DR) | ADDI (BR | CR | DR) (BR | CR | DR) INTEGER |
+                  SUB (BR | CR | DR) (BR | CR | DR) (BR | CR | DR) | SUBI (BR | CR | DR) (BR | CR | DR) INTEGER |
+                  J INTEGER | BEQ (BR | CR | DR) (BR | CR | DR) INTEGER | BNE (BR | CR | DR) (BR | CR | DR) INTEGER |
+                  LW (BR | CR | DR) (BR | CR | DR) INTEGER | SW (BR | CR | DR) (BR | CR | DR) INTEGER | WRITE (BR | CR | DR)
