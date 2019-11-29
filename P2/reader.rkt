99e4622ad37e186516fac0955dab072daa8a1e01
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2019-11-29 14:50:23 -0300

    Adicionando o leitor

--- P2/reader.rkt
+++ P2/reader.rkt
@@ -1,1 +1,29 @@
- 
+#lang br/quicklang
+
+(require "parser.rkt")
+
+(define (read-syntax path port)
+  (define parse-tree (parse path (make-tokenizer port)))
+  (define module-datum `(module bf-mod "expander.rkt"
+                          ,parse-tree))
+  (datum->syntax #f module-datum))
+(provide read-syntax)
+
+(require brag/support)
+
+(define-lex-abbrev digits (:+ (char-set "0123456789")))
+
+(define (make-tokenizer port)
+  (define (next-token)
+    (define bf-lexer
+      (lexer
+       ["add" (token 'ADD lexeme)]
+       ["sub" (token 'SUB lexeme)]
+       ["br" (token 'BR lexeme)]
+       ["cr" (token 'CR lexeme)]
+       ["dr" (token 'DR lexeme)]
+       ["write" (token 'WRITE lexeme)]
+       [digits (token 'INTEGER (string->number lexeme))]
+       [any-char (next-token)]))
+    (bf-lexer port))
+  next-token)  
