b8b6c527857db23e0397e2091b081890d362b1cf
Gustavo Oliveira Quinteiro <gustavooquinteiro@outlook.com>
2019-11-29 18:45:11 -0300

    Desenvolvendo as funções dos comandos MIPS

--- P2/expander.rkt
+++ P2/expander.rkt
@@ -1,33 +1,28 @@
 #lang br/quicklang
 
 (define-macro (mips-module-begin PARSE-TREE)
-  #'(#%module-begin PARSE-TREE))
+  #'(#%module-begin 'PARSE-TREE))
 
 (provide (rename-out 
             [mips-module-begin #%module-begin]))
 
 
-(define-macro (mips-program OPERATION-OR-LOOP-ARGS ...)
-  #'(void OPERATION-OR-LOOP-ARGS ...))
+(define-macro (mips-program OPERATION-ARGS ...)
+  #'(void OPERATION-ARGS ...))
 
 (provide mips-program)
 
-(define-macro (mips-loop "begin" OPERATION-OR-LOOP-ARGS ... "end")
-  #'(until (zero? (current-byte))
-      OPERATION-OR-LOOP-ARGS ...))
-
-(provide mips-loop)
-
 (define-macro-cases mips-instruction
-  [(mips-instruction "add" "br" INTEGER) #'(increment "br" INTEGER)]
-  [(mips-instruction "add" "cr" INTEGER) #'(increment "cr" INTEGER)]
-  [(mips-instruction "add" "dr" INTEGER) #'(increment "dr" INTEGER)]
-  [(mips-instruction "sub" "br" INTEGER) #'(decrement "br" INTEGER)]
-  [(mips-instruction "sub" "cr" INTEGER) #'(decrement "cr" INTEGER)]
-  [(mips-instruction "sub" "dr" INTEGER) #'(decrement "dr" INTEGER)]
-  [(mips-instruction "fwd" INTEGER) #'(foward-n INTEGER)]
-  [(mips-instruction "rwd" INTEGER) #'(rewind-n INTEGER)]
-  [(mips-instruction WRITE) #'(write)])
+  [(mips-instruction "addi" register1 register2 INTEGER) #'(increment-immediate register1 register2 INTEGER)]
+  [(mips-instruction "subi" register1 register2 INTEGER) #'(decrement-immediate register1 register2 INTEGER)]
+  [(mips-instruction "add" register1 register2 register3) #'(increment register1 register2 register3)]
+  [(mips-instruction "sub" register1 register2 register3) #'(decrement register1 register2 register3)]
+  [(mips-instruction "lw" register1 register2 INTEGER) #'(load-word register1 register2 INTEGER)]
+  [(mips-instruction "sw" register1 register2 INTEGER) #'(store-word register1 register2 INTEGER)]
+  [(mips-instruction "beq" register1 register2 INTEGER) #'(beq register1 register2 INTEGER)]
+  [(mips-instruction "bne" register1 register2 INTEGER) #'(bne register1 register2 INTEGER)]
+  [(mips-instruction "j" INTEGER) #'(jump INTEGER)]
+  [(mips-instruction "write" reg) #'(write reg)])
 
 (provide mips-instruction)
 
@@ -40,19 +35,64 @@
 (define cr 0)
 (define dr 0)
 
-(define (increment register integer)
-  (cond
-    [(eq? register "br") (set! br (+ br integer))]
-    [(eq? register "cr") (set! cr (+ cr integer))]
-    [else (set! dr (+ dr integer))]))
 
-(define (decrement register integer)
-  (cond
-    [(eq? register "br") (set! br (- br integer))]
-    [(eq? register "cr") (set! cr (- cr integer))]
-    [else (set! dr (- dr integer))]))
+(define (next-pc [branch 0][jump #f])
+  (if (jump)
+      (set! pc branch)
+      (set! pc (+ pc 4 branch))
+  ))
 
-(define (write)
-  (write-byte br))
+(define (check-register reg)
+  (cond
+   [(eq? reg "br") br]
+   [(eq? reg "cr") cr]
+   [else dr]))
+
+(define (get-from-memory reg1 integer)
+  (vector-ref memory (+ reg1 integer)))
+
+
+(define (increment-immediate register1 register2 integer)
+  (set! (check-register register2) (+ (check-register register1) integer))
+  (next-pc))
+
+(define (decrement-immediate register1 register2 integer)
+  (set! (check-register register2) (- (check-register register1) integer))
+  (next-pc))
+
+(define (increment register1 register2 register3)
+  (set! (check-register register2) (+ (check-register register1) (check-register register3)))
+  (next-pc))
+
+(define (decrement register1 register2 register3)
+  (set! (check-register register2) (- (check-register register1) (check-register register3)))
+  (next-pc))
+
+(define (load-word register1 register2 integer)
+  (set! (check-register register2) (get-from-memory (check-register register1) integer))
+  (next-pc))
+
+(define (store-word register1 register2 integer)
+  (vector-set! memory (+ (check-register register1) integer) (check-register register2))
+  (next-pc))
+
+(define (beq reg1 reg2 integer)
+  (if (eq? (check-register reg1) (check-register reg2))
+      (next-pc integer)
+      (next-pc)
+   ))
+
+(define (bne reg1 reg2 integer)
+  (if (not (eq? (check-register reg1) (check-register reg2)))
+      (next-pc integer)
+      (next-pc)
+   ))
+
+(define (jump integer)
+  (next-pc integer #t))
+
+(define (write reg)
+  (write-byte (check-register reg))
+  (next-pc))
  
 
